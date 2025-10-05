669f75ede00869846478d585162c9c9ad1bfaaa4
chotu17086 <chotu17086@gmail.com>
2025-10-05 16:10:55 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,11 @@
+require('dotenv').config();  // Load .env variables
+const mongoose = require('mongoose');
+
+const mongoURI = process.env.MONGO_URI;
+
+mongoose.connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log("MongoDB connected");
+}).catch(err => console.log(err));
